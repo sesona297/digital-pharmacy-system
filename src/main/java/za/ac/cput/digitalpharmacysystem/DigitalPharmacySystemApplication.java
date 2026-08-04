7236c38bda8d09e939b3cbf77191c9095625f39b
sesona297 <240773365@mycput.ac.za>
2026-08-04 21:03:38 +0200

    Initial commit: Base project structure

---
+++ src/main/java/za/ac/cput/digitalpharmacysystem/DigitalPharmacySystemApplication.java
@@ -0,0 +1,13 @@
+package za.ac.cput.digitalpharmacysystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalPharmacySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalPharmacySystemApplication.class, args);
+	}
+
+}
