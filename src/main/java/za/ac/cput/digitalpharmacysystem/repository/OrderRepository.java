a71724275933a54a9c591d3e5b27bf9eaa83266a
Spama221447598 <221447598@mcput.ac.za>
2026-08-22 05:09:30 +0200

    Added OrderRepository

---
+++ src/main/java/za/ac/cput/digitalpharmacysystem/repository/OrderRepository.java
@@ -0,0 +1,11 @@
+package za.ac.cput.digitalpharmacysystem.repository;
+/*
+ * Sibulele Pama
+ * 221447498
+ */
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends IOrderRepository {
+}
